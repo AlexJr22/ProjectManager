4489adb60d7232a1a1510a4f4be490dcc7d41d5a
Alex Junior <alexjr2oficial@gmail.com>
2024-12-06 11:55:12 -0300

    improving tables of database

---
+++ ProjetcManager.API/Migrations/20241206142741_ModifyUserModel2.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetcManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyUserModel2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectModelUserModel_Projects_TasksId",
+                table: "ProjectModelUserModel");
+
+            migrationBuilder.RenameColumn(
+                name: "TasksId",
+                table: "ProjectModelUserModel",
+                newName: "ProjectsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectModelUserModel_Projects_ProjectsId",
+                table: "ProjectModelUserModel",
+                column: "ProjectsId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectModelUserModel_Projects_ProjectsId",
+                table: "ProjectModelUserModel");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjectsId",
+                table: "ProjectModelUserModel",
+                newName: "TasksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectModelUserModel_Projects_TasksId",
+                table: "ProjectModelUserModel",
+                column: "TasksId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
