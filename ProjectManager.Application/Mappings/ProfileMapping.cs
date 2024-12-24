153bbdb688075bb6da550cf100e8b35846e3774e
Alex Junior <alexjr2oficial@gmail.com>
2024-12-24 19:03:09 -0300

    creating mappings for DTOs

---
+++ ProjectManager.Application/Mappings/ProfileMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjectManager.Application.DTOs.Project;
+using ProjectManager.Application.DTOs.Task;
+using ProjectManager.Domain.Entities;
+
+namespace ProjectManager.Application.Mappings;
+
+public class ProfileMapping : Profile
+{
+    public ProfileMapping()
+    {
+        CreateMap<ProjectDTO, ProjectModel>().ReverseMap();
+        CreateMap<TaskDTO, ProjectModel>().ReverseMap();
+    }
+}
