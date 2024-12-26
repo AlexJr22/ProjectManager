c70efa42b7fa3df523d4a208fefd6ff5b2f92927
Alex Junior <alexjr2oficial@gmail.com>
2024-12-26 18:02:53 -0300

    formating the code

--- ProjetcManager.API/Program.cs
+++ ProjetcManager.API/Program.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,46 +12,54 @@ using ProjetcManager.API.Repositories;
 using ProjetcManager.API.Repositories.interfaces;
 using ProjetcManager.API.Services;
 using ProjetcManager.API.Services.Interfaces;
-using System.Text;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers()
-    .AddJsonOptions(op => op.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder
+    .Services
+    .AddControllers()
+    .AddJsonOptions(
+        op => op.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+    );
 
 // swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectManager.API", Version = "v1" });
+builder
+    .Services
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectManager.API", Version = "v1" });
 
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
+        c.AddSecurityDefinition(
+            "Bearer",
             new OpenApiSecurityScheme
             {
-                Reference = new OpenApiReference
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+            }
+        );
+        c.AddSecurityRequirement(
+            new OpenApiSecurityRequirement
+            {
                 {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
                 }
-            },
-
-            Array.Empty<string>()
-        }
+            }
+        );
     });
-});
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
@@ -58,42 +68,48 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
 // Configuring Identity and Ef Core
-builder.Services
+builder
+    .Services
     .AddIdentity<UserModel, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
-string StringConnection = builder.Configuration.GetConnectionString("SQLite")
+string StringConnection =
+    builder.Configuration.GetConnectionString("SQLite")
     ?? throw new ArgumentException("Invalid String Connection!");
 
 builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(StringConnection));
 
-var Key = builder.Configuration["JWT:Key"]
-    ?? throw new ArgumentException("Invalid Secret Key!");
+var Key = builder.Configuration["JWT:Key"] ?? throw new ArgumentException("Invalid Secret Key!");
 
 // authentication services
-builder.Services.AddAuthentication(op =>
-{
-    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(op =>
-{
-    op.SaveToken = true;
-    op.RequireHttpsMetadata = false;
-    op.TokenValidationParameters = new TokenValidationParameters()
+builder
+    .Services
+    .AddAuthentication(op =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ClockSkew = TimeSpan.Zero,
-        ValidAudience = builder.Configuration["JWT:ValidAudiance"],
-        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes((Key)))
-    };
-});
+        op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(op =>
+    {
+        op.SaveToken = true;
+        op.RequireHttpsMetadata = false;
+        op.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidAudience = builder.Configuration["JWT:ValidAudiance"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes((Key)))
+        };
+    });
 
-builder.Services.AddAuthorizationBuilder()
+builder
+    .Services
+    .AddAuthorizationBuilder()
     .AddPolicy("AdminRequired", policy => policy.RequireRole("Admin"));
 
 var app = builder.Build();
