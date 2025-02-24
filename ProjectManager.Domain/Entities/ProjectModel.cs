a07a4079438742a960c51440f2b2aedb608f4d21
Alex Junior <alexjr2oficial@gmail.com>
2025-02-24 18:12:27 -0300

    modifying domain entities

--- ProjectManager.Domain/Entities/ProjectModel.cs
+++ ProjectManager.Domain/Entities/ProjectModel.cs
@@ -6,4 +6,33 @@ public sealed class ProjectModel
     public string? ProjectName { get; private set; }
     public ICollection<TaskModel>? Tasks { get; private set; }
     //public ICollection<UserModel>? Users { get; private set; }
+
+    public ProjectModel(string projectName, int id)
+    {
+        ProjectName = NameValidation(projectName);
+        Id = id;
+    }
+
+    public void Update(string? projectName, ICollection<TaskModel>? tasks)
+    {
+        ProjectName = projectName;
+        Tasks = tasks;
+    }
+
+    private string NameValidation(string name)
+    {
+        if (name.Length < 3)
+            throw new ArgumentException(
+                "The project name cannot have less than three letters!",
+                nameof(name)
+            );
+
+        if (name.Length > 25)
+            throw new ArgumentException(
+                "The project name cannot have more than twenty-five letters!",
+                nameof(name)
+            );
+
+        return name;
+    }
 }
--- ProjectManager.Domain/Entities/TaskModel.cs
+++ ProjectManager.Domain/Entities/TaskModel.cs
@@ -1,11 +1,47 @@
-﻿namespace ProjectManager.Domain.Entities;
+﻿using System.Text.RegularExpressions;
+
+namespace ProjectManager.Domain.Entities;
 
 public sealed class TaskModel
 {
     public int Id { get; private set; }
-    public string? TaskName { get; private set; }
+    public string TaskName { get; private set; }
     public string? TaskDescription { get; private set; }
     public bool TaskStatus { get; private set; }
     public int? ProjectId { get; private set; }
     public ProjectModel? Project { get; private set; }
+
+    public TaskModel(string taskName, string? taskDescription, int? projectId, int id)
+    {
+        TaskName = NameValidation(taskName);
+        TaskDescription = taskDescription;
+        TaskStatus = false;
+        ProjectId = projectId;
+        Id = id;
+    }
+
+    private string NameValidation(string name)
+    {
+        if (name.Length < 3)
+            throw new ArgumentException(
+                "The name cannot have less than three letters!",
+                nameof(name)
+            );
+
+        if (name.Length > 25)
+            throw new ArgumentException(
+                "The name cannot have more than twenty-five letters!",
+                nameof(name)
+            );
+
+        return name;
+    }
+
+    public void Update(string taskName, string? taskDescription, int? projectId, bool taskStatus)
+    {
+        TaskName = taskName;
+        TaskDescription = taskDescription;
+        ProjectId = projectId;
+        TaskStatus = taskStatus;
+    }
 }
