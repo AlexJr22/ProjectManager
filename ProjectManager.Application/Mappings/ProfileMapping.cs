b7c45ca2c752da48707641ed0ce3d5f8b0d1748b
Alex Junior <alexjr2oficial@gmail.com>
2025-03-20 11:43:20 -0300

    chaging the mappings

--- ProjectManager.Application/Mappings/ProfileMapping.cs
+++ ProjectManager.Application/Mappings/ProfileMapping.cs
@@ -2,6 +2,7 @@
 using ProjectManager.Application.DTOs.Project;
 using ProjectManager.Application.DTOs.Task;
 using ProjectManager.Domain.Entities;
+using System.Globalization;
 
 namespace ProjectManager.Application.Mappings;
 
@@ -10,16 +11,22 @@ public class ProfileMapping : Profile
     public ProfileMapping()
     {
         // project mapping
-        CreateMap<ProjectDTO, ProjectModel>().ReverseMap();
-        CreateMap<CreatingProjectDTO, ProjectModel>().ReverseMap();
-        CreateMap<UpdateProjectDTO, ProjectModel>().ReverseMap();
+        CreateMap<ProjectModel, ProjectDTO>()
+            .ForMember(
+                dest => dest.CreateAt,
+                opt => opt.MapFrom(src => src.CreateAt.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture))
+            )
+            .ReverseMap();
+
+        CreateMap<ProjectModel, CreatingProjectDTO>().ReverseMap();
+        CreateMap<ProjectModel, UpdateProjectDTO>().ReverseMap();
         CreateMap<UpdateProjectDTO, ProjectDTO>().ReverseMap();
 
-        
         // task mapping
-        CreateMap<TaskDTO, TaskModel>().ReverseMap();
-        CreateMap<CreatingTaskDTO, TaskModel>().ReverseMap();
-        CreateMap<UpdateTaskDTO, TaskModel>().ReverseMap();
+        CreateMap<TaskDTO, TaskModel>()
+            .ReverseMap();
+        CreateMap<TaskModel, CreatingTaskDTO>().ReverseMap();
+        CreateMap<TaskModel, UpdateTaskDTO>().ReverseMap();
         CreateMap<UpdateTaskDTO, TaskDTO>().ReverseMap();
     }
 }
