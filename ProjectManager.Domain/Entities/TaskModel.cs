04069b15065b1261bb0fc8a0f8615505733696e3
Alex Junior <alexjr2oficial@gmail.com>
2025-03-28 10:00:28 -0300

    improving TaskModel

--- ProjectManager.Domain/Entities/TaskModel.cs
+++ ProjectManager.Domain/Entities/TaskModel.cs
@@ -5,21 +5,45 @@ namespace ProjectManager.Domain.Entities;
 public sealed class TaskModel
 {
     public int Id { get; private set; }
-    public string TaskName { get; private set; }
+    public string? TaskName { get; private set; }
     public string? TaskDescription { get; private set; }
     public bool TaskStatus { get; private set; }
+    public DateTime CreateAt { get; private set; }
     public int? ProjectId { get; private set; }
     public ProjectModel? Project { get; private set; }
 
-    private TaskModel() { }
+    private TaskModel()
+    {
+        CreateAt = DateTime.UtcNow;
+    }
 
-    public TaskModel(string taskName, string? taskDescription, int? projectId, int id)
+    public TaskModel(
+        string taskName,
+        string? taskDescription,
+        int? projectId,
+        int id,
+        ProjectModel? project
+    )
     {
         TaskName = NameValidation(taskName);
         TaskDescription = taskDescription;
         TaskStatus = false;
         ProjectId = projectId;
         Id = id;
+        Project = project;
+        CreateAt = DateTime.UtcNow;
+    }
+
+    public void Update(string taskName, string? taskDescription, int? projectId, bool taskStatus)
+    {
+        if (taskName is not null)
+            TaskName = taskName;
+
+        if (projectId is not null)
+            ProjectId = projectId;
+
+        TaskStatus = taskStatus;
+        TaskDescription = taskDescription;
     }
 
     private string NameValidation(string name)
@@ -37,13 +61,5 @@ public sealed class TaskModel
             );
 
         return name;
-    }
-
-    public void Update(string taskName, string? taskDescription, int? projectId, bool taskStatus)
-    {
-        TaskName = taskName;
-        TaskDescription = taskDescription;
-        ProjectId = projectId;
-        TaskStatus = taskStatus;
     }
 }
