58213972bfdd67b1bbd77013d39866cf28f26b01
Alex Junior <alexjr2oficial@gmail.com>
2025-04-11 11:34:15 -0300

    creating a generic mapper

---
+++ ProjectManager.Application/Mappings/Mapper.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectManager.Application.Mappings;
+
+public static class Mapper
+{
+    public static Target Map<Target, Souce>(Souce souce)
+        where Target : new()
+    {
+        var target = new Target();
+
+        var souceProperty = typeof(Souce).GetProperties();
+        var targetProperty = typeof(Target).GetProperties();
+
+        foreach (var property in souceProperty)
+        {
+            var targetProp = targetProperty.FirstOrDefault(
+                p => p.Name == property.Name && p.PropertyType == property.PropertyType
+            );
+
+            if (targetProp is not null && targetProp.CanWrite)
+            {
+                var value = property.GetValue(souce);
+                targetProp.SetValue(target, value);
+            }
+        }
+
+        return target;
+    }
+}
