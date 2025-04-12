26ba7d3dd3b04ee5023d96b15ae490d146655869
Alex Junior <alexjr2oficial@gmail.com>
2025-04-12 10:38:13 -0300

    improving the ProjectDTO

--- ProjectManager.Application/DTOs/Project/ProjectDTO.cs
+++ ProjectManager.Application/DTOs/Project/ProjectDTO.cs
@@ -4,5 +4,5 @@ public record class ProjectDTO
 {
     public int Id { get; set; }
     public string? ProjectName { get; set; }
-    public string CreateAt { get; set; } = null!;
+    public DateTime CreateAt { get; set; }
 }
