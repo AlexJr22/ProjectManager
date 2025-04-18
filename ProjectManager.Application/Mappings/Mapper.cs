f051b113dbb424799ceaaa2a7c75b75cb51d8997
Alex Junior <alexjr2oficial@gmail.com>
2025-04-18 12:40:03 -0300

    improving mapper

--- ProjectManager.Application/Mappings/Mapper.cs
+++ ProjectManager.Application/Mappings/Mapper.cs
@@ -1,24 +1,41 @@
-﻿namespace ProjectManager.Application.Mappings;
+﻿using System.Reflection;
+
+namespace ProjectManager.Application.Mappings;
 
 public static class Mapper
 {
-    public static TTarget Map<TTarget, TSouce>(TSouce souce)
+    public static TTarget Map<TTarget, TSource>(TSource source)
         where TTarget : new()
     {
         var target = new TTarget();
 
-        var souceProperty = typeof(TSouce).GetProperties();
-        var targetProperty = typeof(TTarget).GetProperties();
+        var sourceProperties = typeof(TSource).GetProperties();
+        var targetProperties = typeof(TTarget).GetProperties();
 
-        foreach (var property in souceProperty)
+        foreach (var sourceProp in sourceProperties)
         {
-            var targetProp = targetProperty.FirstOrDefault(
-                p => p.Name == property.Name && p.PropertyType == property.PropertyType
-            );
+            var targetProp = targetProperties.FirstOrDefault(p => p.Name.Equals(sourceProp.Name));
 
-            if (targetProp is not null && targetProp.CanWrite)
+            if (targetProp is null || !targetProp.CanWrite)
+                continue;
+
+            var sourceValue = sourceProp.GetValue(source);
+
+            if (
+                IsCollection(sourceProp.PropertyType, out var sourceItemType)
+                && IsCollection(targetProp.PropertyType, out var targetItemType)
+            )
             {
-                var value = property.GetValue(souce);
+                var method = typeof(Mapper)
+                    .GetMethod(nameof(MapList), BindingFlags.NonPublic | BindingFlags.Static)!
+                    .MakeGenericMethod(targetItemType, sourceItemType);
+
+                var mappedList = method.Invoke(null, new object[] { sourceValue! });
+                targetProp.SetValue(target, mappedList);
+            }
+            else if (targetProp.PropertyType == sourceProp.PropertyType)
+            {
+                var value = sourceProp.GetValue(source);
                 targetProp.SetValue(target, value);
             }
         }
@@ -33,5 +50,43 @@ public static class Mapper
         {
             yield return Map<TTarget, TSource>(item);
         }
+    }
+
+    private static bool IsCollection(Type type, out Type itemType)
+    {
+        if (
+            type.IsGenericType
+            && typeof(IEnumerable<>).IsAssignableFrom(type.GetGenericTypeDefinition())
+        )
+        {
+            itemType = type.GetGenericArguments()[0];
+            return true;
+        }
+
+        var iface = type.GetInterfaces()
+            .FirstOrDefault(
+                i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)
+            );
+
+        if (iface is not null && type != typeof(string))
+        {
+            itemType = iface.GetGenericArguments()[0];
+            return true;
+        }
+
+        itemType = null!;
+        return false;
+    }
+
+    private static List<TTarget> MapList<TTarget, TSource>(IEnumerable<object> sourceList)
+        where TTarget : new()
+    {
+        var result = new List<TTarget>();
+        foreach (var item in sourceList.Cast<TSource>())
+        {
+            result.Add(Map<TTarget, TSource>(item));
+        }
+
+        return result;
     }
 }
